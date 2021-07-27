dcf2f59923e4d771bf3bf7c5f64c71179550f8dc
Jet-Gill-Lee <jetgilllee@hotmail.com>
2021-07-27 15:52:02 +1000

    initial setup

--- .history/routes/index_20210727140540.js
+++
@@ -1,7 +0,0 @@
-const { response } = require('express')
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req,res) => {
-  res.send('hello')
-})
--- .history/routes/index_20210727141213.js
+++
@@ -1,9 +0,0 @@
-const { response } = require('express')
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req,res) => {
-  res.render('index')
-})
-
-module.exports = router
